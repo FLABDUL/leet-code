9697644a7f5ff8a8d1d52735199d3e2b15054602
Abdul Hakim Norazman <ahnorazman@gmail.com>
2025-01-28 20:57:18 +0000

    Number of islands problem and solution

---
+++ src/problems/graph_general/numer_of_islands/NumberOfIslands.java
@@ -0,0 +1,21 @@
+//package problems.graph_general.numer_of_islands;
+//
+//public class NumberOfIslands {
+//    public static void main(String[] args) {
+//        char[][] grid = {
+//          {'1','1','1','1','0'},
+//          {'1','1','0','1','0'},
+//          {'1','1','0','0','0'},
+//          {'0','0','0','0','0'}
+//        };
+//        NumberOfIslands numberOfIslands = new NumberOfIslands();
+//        System.out.println("result: " + numberOfIslands.numIslands(grid));
+//    }
+//
+//    public int numIslands(char[][] grid) {
+//
+//
+//
+//        return 0;
+//    }
+//}
